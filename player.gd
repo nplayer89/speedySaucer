55d8a4fd867e338b45641496245d141c958c8f5f
nplayer89 <n.player89@gmail.com>
2023-10-08 19:54:58 -0600

    update player movement with force variable

--- player.gd
+++ player.gd
@@ -10,11 +10,12 @@ func _process(delta):
 	pass
 
 func _physics_process(delta):
+	var force = 1000
 	if Input.is_action_pressed("move_right"):
-		apply_force(Vector2(25, 0))
+		apply_force(Vector2(force, 0))
 	if Input.is_action_pressed("move_left"):
-		apply_force(Vector2(-25, 0))
+		apply_force(Vector2(-force, 0))
 	if Input.is_action_pressed("move_down"):
-		apply_force(Vector2(0, 25))
+		apply_force(Vector2(0, force))
 	if Input.is_action_pressed("move_up"):
-		apply_force(Vector2(0, -25))
+		apply_force(Vector2(0, -force))
