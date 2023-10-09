1fd5b56000e2f1218fa689146518a6deea561140
nplayer89 <n.player89@gmail.com>
2023-10-09 12:51:46 -0600

    camera follows player

--- player.gd
+++ player.gd
@@ -13,8 +13,8 @@ func _process(delta):
 func _physics_process(delta):
 	player_movement()
 
+# Player movement controller
 func player_movement():
-		# player movement
 	if Input.is_action_pressed("move_right"):
 		apply_force(Vector2(force, 0))
 	if Input.is_action_pressed("move_left"):
